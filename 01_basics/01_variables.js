66223a266ddab4a95758c8082a421aff47eb7dfe
shrimudliyar <shrimudliyar90@gmail.com>
2024-12-05 03:14:29 +0000

    variables done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accId = 1234567
+let accEmail = "shri@g.com"
+var accPassword = "1234567890"
+accCity="Mumbai" 
+// with using let and const don't declare any variables , and don't use var cozz of issue in block scope and functional scope
+console.log(accId)
+console.table([accId,accEmail,accPassword , accCity])
