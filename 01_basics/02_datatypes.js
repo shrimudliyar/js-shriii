ab105d753972ea8b2300bcc152146efdd406013b
shrimudliyar <shrimudliyar90@gmail.com>
2024-12-05 03:28:53 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strick"; // treat all js code has newer version 
+
+// alert(3+3) //this is used in browser we are using nodejs
+
+let Name = "Shrii"
+let age = 18
+let  isLoggedIn = true 
+
+// datatypes : primitive 
+// number -> 2 to the power of 53 approx
+// bigint
+// string = '' / ""
+// boolean = true / false
+// null -> standalone value
+// undifined 
+// symbol -> unique
+
+// object 
+console.log(typeof null); // null is an object , and unifined is a type
+
