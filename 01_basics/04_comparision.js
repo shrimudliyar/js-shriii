78aac809309e152b783c8a51540dc12589f3de20
shrimudliyar <shrimudliyar90@gmail.com>
2024-12-07 13:00:11 +0000

    comparision done

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,23 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// always compare two same datatypes 
+// console.log("2" > 1); // -> true
+// console.log("02" > 1); // -> true
+
+// console.log(null > 0 ); //-> false
+// console.log(null == 0 ); // -> false
+// console.log(null >= 0); // -> ture
+// console.log(null <= 0); // -> ture
+
+// console.log(undefined > 0 ); //-> false
+// console.log(undefined == 0 ); // -> false
+// console.log(undefined >= 0); // -> false
+// console.log(undefined <= 0); // -> false
+
+// strick check (===)  in this it also compares the datatypes of both
+console.log();
+
