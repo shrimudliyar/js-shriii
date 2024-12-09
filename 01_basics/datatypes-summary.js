fd62af63ec9c83b4032c95bdb970f3db2f3e0829
shrimudliyar <shrimudliyar90@gmail.com>
2024-12-09 12:41:27 +0000

    datatypes summary done

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,22 @@
+// # Primitive Datatype
+
+// 7 types : STring, Number , Boolean , Null , undifined , Symbol , bigint ;
+
+// javascript  is dynamic 
+
+// Reference (Non primitive )
+
+// Array , Objects , Functions
+
+const arrayy = ["spiderman" , "ironman" , "Thorr"]
+
+let myObj = {
+    name : "Shrinivas",
+    age : 18,
+}
+
+const myFunction = function(){
+    console.log("Hey Shriiii !!!!");    
+}
+
+console.log(typeof myFunction);
