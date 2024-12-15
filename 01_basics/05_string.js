eebff6bf5b80cd468cb68e2a8fa3c6276f87d669
shrimudliyar <shrimudliyar90@gmail.com>
2024-12-15 12:52:13 +0000

    str done

---
+++ 01_basics/05_string.js
@@ -0,0 +1,9 @@
+const name = "Shrinivas"
+const age = 18
+ 
+// this method is not readable and not modern
+// console.log(name + " age is " + age); -> Shrinivas age is 18
+
+
+// should use this method 
+console.log(`Hello my name is ${name} and my age is ${age}`);
