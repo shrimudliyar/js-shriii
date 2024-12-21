c08b5a0b532dc4c65f4fbe140efdb5725f2b0e6a
shrimudliyar <shrimudliyar90@gmail.com>
2024-12-21 15:56:19 +0000

    math done

--- 01_basics/06_numAndMaths.js
+++ 01_basics/06_numAndMaths.js
@@ -1,6 +1,51 @@
 // Numbers
 const score = 500 
-console.log(score);
+// console.log(score);
 
 const balance = new Number(100) 
-console.log(balance);
+// console.log(balance);
+
+// console.log((balance.toString().length));
+
+const lulu = 123.45678
+// console.log(lulu.toPrecision(4));
+
+const hundreds = 10000000
+// console.log(hundreds.toLocaleString()) //->10,000,000
+// console.log(hundreds.toLocaleString('en-IN')) //->1,00,00,000
+
+
+// **************MATHS*************
+// console.log(Math); //->Object [Math] {}
+
+// console.log(Math.abs(-4));// abs = absolute value , it changes negative values in positive , output = 4
+
+// console.log(Math.round(7.23)); // roundoff's the value 
+// types of rounding off valiueu 
+// console.log(Math.ceil(7.1)); // roundoff's to greater value 
+// console.log(Math.floor(7.8)); // roundoff's to smaller value 
+
+// to find min or max value 
+// console.log(Math.min(1,5,9,3,6));
+// console.log(Math.max(1,5,9,3,6));
+
+
+// console.log(Math.random());
+
+// console.log((Math.random()*10)+1);
+
+// console.log(Math.floor((Math.random()*10)+1));
+
+const min = 1;
+const max = 6;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
