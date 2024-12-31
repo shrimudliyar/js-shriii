39be57e98132c3c20f1b5c36423cac58ac5d42db
shrimudliyar <shrimudliyar90@gmail.com>
2024-12-31 11:07:08 +0000

    obj 2 done

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -28,4 +28,31 @@ const obj2 = { 3: "a", 4: "b"}
 // const  obj3 = Object.assign({},obj1 , obj2)
 
 const obj3 = {...obj1,...obj2}
-console.log(obj3);
+// console.log(obj3);
+
+const users = [
+    {
+        id : 1 ,
+        email : "sh@gmail.com",
+    },
+    {
+        id : 1 ,
+        email : "sh@gmail.com",
+    },
+     {
+        id : 1 ,
+        email : "sh@gmail.com",
+    },
+     {
+        id : 1 ,
+        email : "sh@gmail.com",
+    },
+]
+users[1].email
+
+console.log(tinderUser);
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedin'));
